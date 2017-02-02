0ac7b02c532685ec8f3bf7fdbc2f6e5588150556
Bryan Quinn <bryan.quinn17@gmail.com>
2017-02-02 00:47:56 -0500

    Begin to add api methods in views

--- intuitRitChallenge/views.py
+++ intuitRitChallenge/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from intuitRitChallenge import models
+from django.http import JsonResponse
+from django.core.serializers import serialize
 
 
 def home_screen(request):
@@ -10,7 +13,21 @@ def charts(request):
 
 
 def tables(request):
-    return render(request, 'tables.html', {'view': "tables"})
+    transactions = api_transactions(request)
+    return render(request, 'tables.html', {'view': "tables", 'data': transactions})
+
+
+def api_transactions(request):
+    transactions = "{'id': \"No data found\"}"
+    if request.method == "GET":
+        try:
+            account = request.GET["account"]
+        except KeyError:
+            account = ""
+
+        transactions = serialize("json", models.Transaction.objects.filter(owner=account))
+
+    return JsonResponse(transactions)
 
 
 def not_found(request):
