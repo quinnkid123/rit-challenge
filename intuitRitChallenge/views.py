ef1af5471f0a45ec327cc4717095690528c74ade
Bryan Quinn <bryan.quinn17@gmail.com>
2017-02-03 22:38:10 -0500

    Add api call for matchmaking

--- intuitRitChallenge/views.py
+++ intuitRitChallenge/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from intuitRitChallenge.models import *
 from django.http import JsonResponse, HttpRequest
-from django.core.serializers import serialize
-import json
+import decimal
 
 
 def home_screen(request):
@@ -24,10 +23,7 @@ def api_transactions(request, account):
     json_data = []
     if request.method == "GET":
 
-        if "account" in request.GET:
-            user_id = request.GET["account"]
-        else:
-            user_id = account
+        user_id = account
 
         transactions = Transactions.objects.filter(owner=user_id)
         for transaction in transactions:
@@ -44,10 +40,8 @@ def api_transactions(request, account):
 def api_features(request, account):
     json_data = {}
     if request.method == "GET":
-        if "account" in request.GET:
-            user_id = request.GET["account"]
-        else:
-            user_id = account
+
+        user_id = account
 
         features = Features.objects.filter(owner=user_id)
         for feature in features:
@@ -63,7 +57,61 @@ def api_features(request, account):
                 'favorite_restaurant': str(feature.first_top_purchase)
             }
 
-    return JsonResponse(json_data, safe=False)
+    return JsonResponse(json_data)
+
+
+def cupid(soul, mate):
+    """
+    The ultimate matchmaking algorithm
+    :param soul: person 1
+    :param mate: person 2
+    :return: number between 0 and 1 represent the percentatge match
+    """
+    # Map all of the user's transactions from vendor to the amount spent there
+    soul_transactions = {}
+    for transaction in Transactions.objects.filter(owner=soul):
+        if transaction.vendor_id.name in soul_transactions:
+            soul_transactions[transaction.vendor_id.name] += abs(transaction.amount)
+        else:
+            soul_transactions[transaction.vendor_id.name] = abs(transaction.amount)
+
+    mate_transactions = {}
+    for transaction in Transactions.objects.filter(owner=mate):
+        if transaction.vendor_id.name in mate_transactions:
+            mate_transactions[transaction.vendor_id.name] += abs(transaction.amount)
+        else:
+            mate_transactions[transaction.vendor_id.name] = abs(transaction.amount)
+
+    percent_match = decimal.Decimal(0.0)
+    count = 0
+
+    # Find all common vendor expense
+    for vendor in soul_transactions.keys():
+        if vendor in mate_transactions.keys():
+            # Determine how similar the amount spent is
+            if soul_transactions[vendor] / mate_transactions[vendor] < 1:
+                ratio = soul_transactions[vendor] / mate_transactions[vendor]
+            else:
+                ratio = mate_transactions[vendor] / soul_transactions[vendor]
+            # Multiply by the uniqueness of the vendor
+            ratio *= decimal.Decimal((101 - Vendors.objects.get(name=vendor).number_of_buyers) / 100)
+            percent_match += ratio
+            count += 1
+
+    return percent_match / count  # find the average vendor interest ratio
+
+
+def api_matchmaker(request, account, match):
+    json_data = {}
+    if request.method == "GET":
+        score = cupid(account, match)
+        json_data = {
+            "account": account,
+            "match": match,
+            "score": str(score)
+        }
+
+    return JsonResponse(json_data)
 
 
 def not_found(request):
