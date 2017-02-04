6afc12b445065de1e6a89c280f056e96694e26f1
Bryan Quinn <bryan.quinn17@gmail.com>
2017-02-03 22:03:44 -0500

    Create basic api methods

--- intuitRitChallenge/views.py
+++ intuitRitChallenge/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 from intuitRitChallenge.models import *
 from django.http import JsonResponse, HttpRequest
 from django.core.serializers import serialize
+import json
 
 
 def home_screen(request):
-    return render(request, 'index.html', {'view': "dashboard"})
+    return render(request, 'charts.html', {'view': "charts"})
 
 
 def charts(request):
@@ -19,37 +20,50 @@ def tables(request):
     return render(request, 'tables.html', {'view': "tables", 'data': transactions})
 
 
-def api_transactions(request):
-    transactions = "{'id': \"No data found\"}"
+def api_transactions(request, account):
+    json_data = []
     if request.method == "GET":
-        try:
-            account = request.GET["account"]
-        except KeyError:
-            account = ""
 
-        transactions = Transactions.objects.filter(owner=int(account))
-        json = []
+        if "account" in request.GET:
+            user_id = request.GET["account"]
+        else:
+            user_id = account
+
+        transactions = Transactions.objects.filter(owner=user_id)
         for transaction in transactions:
-            json.append({
-                'date': transaction.date,
+            json_data.append({
+                'date': str(transaction.date),
                 'vendor': transaction.vendor,
-                'amount': transaction.amount
+                'amount': str(transaction.amount)
             })
 
-    return transactions
+        # json_data = json.dumps(json_data)
+    return JsonResponse(json_data, safe=False)
 
 
-def api_features(request):
-    features = "{'id': \"No data found\"}"
+def api_features(request, account):
+    json_data = {}
     if request.method == "GET":
-        try:
-            account = request.GET["account"]
-        except KeyError:
-            account = ""
+        if "account" in request.GET:
+            user_id = request.GET["account"]
+        else:
+            user_id = account
 
-        features = serialize("json", Features.objects.filter(owner=account))
+        features = Features.objects.filter(owner=user_id)
+        for feature in features:
+            json_data = {
+                'salary': str(feature.salary),
+                'spending': str(feature.spending),
+                'first_top_purchase': str(feature.first_top_purchase),
+                'second_top_purchase': str(feature.second_top_purchase),
+                'third_top_purchase': str(feature.third_top_purchase),
+                'has_child': str(feature.has_child),
+                'is_sports_fan': str(feature.is_sports_fan),
+                'was_recently_divorced': str(feature.was_recently_divorced),
+                'favorite_restaurant': str(feature.first_top_purchase)
+            }
 
-    return JsonResponse(features)
+    return JsonResponse(json_data, safe=False)
 
 
 def not_found(request):
